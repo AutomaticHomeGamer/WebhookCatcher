9866ceabba639d31d58afe98afcd0e9cb2957c84
AutomaticHomeGamer <grwoodford@mac.com>
2021-09-18 18:08:40 +0000

    Made changes to simplify database code and prevent spawning threads

--- README.md
+++ README.md
@@ -6,12 +6,19 @@ A GET to localhost:8000/webhook/:name/display will show all entries sent up unti
 
 A GET to localhost:8000/webhook/:name/delete will clear all entries under that name.
 
+A GET to localhost:8000/list will show all existing webhooks
+
+A GET to localhost:8000/webhook/:name/count will give you a count of how many entries there are.
+
+A GET to localhost:8000/webhook/:name/count/:startTime/:duration will give you a count of how many entries have been made within :duration seconds after :startTime(milliseconds since Unix Epoch)
+IE: localhost:8000/webhook/testWebhook/count/1631979950610/30 would get all entries made within the 30 second window following 1631979950610  You can use the time field on any entry as :startTime
+
 You can have multiple webhooks at a time. IE localhost:8000/webhook/TestRun1, localhost:8000/webhook/TestRun2, 
 localhost:8000/webhook/TestRun3, etc. They can be accessed and deleted independantly. 
 
 If you don't need to keep a running log, but just want a quick check of the latest data sent you can use 
 localhost:8000/endpoint  This doesn't need to be manually cleared out and only shows the most recent data
-passed to it parsed as JSON. 
+passed to it parsed as JSON. Accepts POST and GET
 
 A Dockerfile and Docker-compose.yml have been provided for your convenience. 
 docker build -t YourNameHere/webhook .
--- Webhook.js
+++ Webhook.js
@@ -10,6 +10,9 @@ var http = require('http');
 //var https = require('https');
 var MongoClient = require('mongodb').MongoClient;
 var sanitize = require('mongo-sanitize');
+const pug = require('pug');
+app.set('view engine', "pug");
+
 
 var myArgs = process.argv.slice(2);
 console.log(`MyArgs: ${myArgs}`);
@@ -20,8 +23,11 @@ if(myArgs[1]){
         portServer = myArgs[1];
 }
 
-var mongoConnectionURL = `mongodb://mongo:${mongoPort}/Webhook`;
+const mongoConnectionURL = `mongodb://webhookMongo:${mongoPort}/Webhook`;
+const  mongoConnectionURL2 = `mongodb://webhookMongo:${mongoPort}`;
 //The //mongo comes from the docker-compose.yml 
+
+var dbPool;
 
 secretEndpointData = {
     "name":"Trogdor",
@@ -36,6 +42,7 @@ app.post("/endpoint", function(req, res){
         var obj = JSON.parse(req.body)
         secretEndpointData = JSON.stringify(obj);
         res.send("204 No Content");
+	res.end();
     }
     catch(e){
         res.send(e);
@@ -43,71 +50,103 @@ app.post("/endpoint", function(req, res){
     res.end();
 });
 
+app.get("/list", async function(req, res){
+	const hookList = await MongoDB.db("Webhook").collection("hooks").distinct("hook");
+	res.render("list.pug", {"names":hookList});
+	res.end();
+});
 
-app.post("/webhook/:name", function(req, res){
+app.post("/webhook/:name", async function(req, res){
+        console.log(`Webhook hit: ${req.params.name}`);
+	
+	name = sanitize(req.params.name)
+	var ts = Date.now();
+	const result = await MongoDB.db("Webhook").collection("hooks").insertOne({time: ts, hook: name, headers: req.headers, data: sanitize(req.body)} );
+	if(result){
+		res.send("204 No Content");
+		res.end();
+	}else{
+		console.error("No result for add");
+	}
+
+});
+
+app.put("/webhok/:name", async function(req, res){
         console.log(`Webhook hit: ${req.params.name}`);
         callback = function(){
                 res.send("204 No Content");
         }
+	name = sanitize(req.params.name)
+	var ts = Date.now();
+	const result = await MongoDB.db("Webook").collection("hooks").insertOne({time: ts, hook: name, headers: req.headers, data: sanitize(req.body)});
+	if(result){
+		res.send(`204 No Content`);
+		res.end();
+	}else{
+		console.error(`Could not insert record`);
+	}
+});
 
-        MongoClient.connect(mongoConnectionURL , { useUnifiedTopology: true })
-                .then( (db) => {
-                        return db.db().collection('hooks');
-                })
-                .then( (hooks) => {
-                        name = sanitize(req.params.name)
-                        var ts = Date.now();
-                        hooks.insertOne({time: ts, hook: name, headers: req.headers, data: sanitize(req.body)}, function(){ res.send("204 No Content") });
-                } )
-                .catch(function(err){ console.log(err) });
+app.get("/webhook/:name/count", async function(req, res){
+
+	var myCount = await MongoDB.db("Webhook").collection("hooks").countDocuments({hook:sanitize(req.params.name)});
+	res.send(`Count is: ${myCount}`);
+	res.end();
+});
+
+app.get("/webhook/:name/count/:startTime/:duration", async function(req, res){
+	//Finds count of entries :duration seconds after :startTime where :startTime is milliseconds from Unix Epoch
+	name = sanitize(req.params.name);
+        time = parseInt(sanitize(req.params.startTime));
+	oTime = time;
+	duration = parseInt(sanitize(req.params.duration));
+	time += (duration * 1000);
+
+	const result = await MongoDB.db("Webhook").collection("hooks").countDocuments({hook: name, time: {$lt: time} });
+	res.send(`Count ${duration} seconds after ${oTime} is: ${result}`);
+	res.end();
 
 });
 
-app.put("/webhok/:name", function(req, res){
-        console.log(`Webhook hit: ${req.params.name}`);
-        callback = function(){
-                res.send("204 No Content");
-        }
+app.get("/webhook/:name/first", async function(req, res){
+	name = sanitize(req.params.name);
+	const result = await MongoDB.db("Webhook").collection("hooks").find({hook:name}).sort({time:1}).toArray();
+	res.send(result);
+	res.end();
+});
 
-        MongoClient.connect(mongoConnectionURL , { useUnifiedTopology: true })
-                .then( (db) => {
-                        return db.db().collection('hooks');
-                })
-                .then( (hooks) => {
-                        name = sanitize(req.params.name)
-                        var ts = Date.now();
-                        hooks.insertOne({time: ts, hook: name, headers: req.headers, data: sanitize(req.body)}, function(){ res.send("204 No Content") });
-                } )
-                .catch(function(err){ console.log(err) });
+app.get("/webhook/:name/delete", async function(req, res){
+	const result = await MongoDB.db("Webhook").collection("hooks").deleteMany({"hook":sanitize(req.params.name)});
+	res.redirect("/webhook/" + sanitize(req.params.name) + "/display");
 
 });
 
-app.get("/webhook/:name/delete", function(req, res){
-        MongoClient.connect(mongoConnectionURL ,{ useUnifiedTopology: true }, function (err, db) {
-                        if(err) throw err;
-                db.db().collection("hooks").deleteMany({"hook":sanitize(req.params.name)});
-        });
-        res.redirect(req.headers.host + "/webhook/"+req.params.name +"/display");
+app.get("/webhook/:name/display", async function(req, res){
+	const result = await MongoDB.db("Webhook").collection("hooks").find({"hook":sanitize(req.params.name)}).sort({$natural:-1}).toArray();
+	res.send(result);
+	res.end();
 });
 
-app.get("/webhook/:name/display", function(req, res){
-
-        MongoClient.connect(mongoConnectionURL ,{ useUnifiedTopology: true }, function (err, db) {
-                if(err) throw err;
-
-                db.db().collection("hooks", function(err, collection){
-                        db.db().collection("hooks").find({"hook": sanitize(req.params.name)}).sort({$natural:-1}).toArray(function(err,arr){
-                                    res.send(arr);
-                                 });
-                        });
-        });
+app.get("/webhook/:name", function(req, res){
+	res.redirect(301, "/webhook/" + sanitize(req.params.name) + "/display");
 });
+
+const MongoDB = new MongoClient(mongoConnectionURL2, { useUnifiedTopology: true });
+async function DBSetup(){
+	try{
+		await MongoDB.connect();
+	}catch(e){
+		console.error(`Database connection error!\n ${e}`);
+	}finally{
+		//await MongoDB.close();
+	}
+}
 
 const httpServer = http.createServer(app);
 var server = httpServer.listen(portServer, function () {
            var host = server.address().address
            var port = server.address().port
-
+		DBSetup().catch(console.error);
            console.log("Webhook listening at http://%s:%s", host, port)
 });
 
